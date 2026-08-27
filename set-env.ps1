aecc5df0cf6a182854cbc3407f58f80ae68b86bb
Arnav Ashok <arnavaru03@gmail.com>
2026-08-27 20:46:42 +0530

    Update set-env.ps1

--- set-env.ps1
+++ set-env.ps1
@@ -1,5 +1,5 @@
 $env:SPRING_DATASOURCE_URL="jdbc:postgresql://localhost:5432/userdb"
-$env:SPRING_DATASOURCE_USERNAME="postgres"
-$env:SPRING_DATASOURCE_PASSWORD="arnavaru03"
+$env:SPRING_DATASOURCE_USERNAME="username"
+$env:SPRING_DATASOURCE_PASSWORD="your_password"
 $env:JWT_SECRET="shopverse-secret-key-shopverse-secret-key"
 Write-Host "Environment variables for Shopverse have been set for this session." -ForegroundColor Green
